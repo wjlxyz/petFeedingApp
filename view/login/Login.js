5fd5924cb56fe06d7c5892a3b1e77c2d9841805b
jinlong <jinlongwang@etekcity.com.cn>
2020-01-20 15:35:30 +0800

    first Component

---
+++ view/login/Login.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import { Text, View } from 'react-native'
+
+export default class extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Login</Text>
+            </View>
+
+        );
+    }
+}
